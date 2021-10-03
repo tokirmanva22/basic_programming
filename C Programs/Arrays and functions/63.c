c02b0feba9881bd5fa082fb6aadff8b456ee0c94
Tokir Manva <38378125+tokirmanva22@users.noreply.github.com>
2021-10-03 11:43:17 +0530

    Modified
    new

--- C Programs/Arrays and functions/63.c
+++ C Programs/Arrays and functions/63.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 void main()
 {
-   int m[3][3], i, j;
+   int mat[3][3], i, j;
+   int n,m;
+   scanf("%d %d",&n,&m);
    printf("Enter the elements of the matrix:\n");
    for(i=0;i<3;++i)
       for(j=0;j<3;++i)
-      scanf("%d", &m[i][j]);
-   printf("\nMatrix...\n");
-   for(i=0;i<3;++i)
+      scanf("%d", &mat[i][j]);
+   printf("\n...Matrix...\n");
+   for(i=0;i<n;++i)
    {
-      for(j=0;j<3;++j)
-         printf("%d ", m[i][j]);
+      for(j=0;j<m;++j)
+         printf("%d ", mat[i][j]);
       printf("\n");
    }
 }
